ecc7571b9dcc566c981d953afd178360df7c0f5a
Andre Martins Ramos <andremr_dev@proton.me>
2025-02-13 08:20:24 -0300

    CH3 L6 - Shots kill asteroids

--- circleshape.py
+++ circleshape.py
@@ -13,9 +13,6 @@ class CircleShape(pygame.sprite.Sprite):
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
 
-    def collides_with(self, other):
-        return self.position.distance_to(other.position) <= self.radius + other.radius
-    
     def draw(self, screen):
         # sub-classes must override
         pass
@@ -23,3 +20,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
